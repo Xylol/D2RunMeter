abc88899589c5b4c9efbb7d328369288a5a5a2b8
Xylol <3837265+Xylol@users.noreply.github.com>
2026-05-18 01:38:29 +0200

    cosmetics

--- D2.UI/Program.cs
+++ D2.UI/Program.cs
@@ -14,7 +14,7 @@ sealed class Program
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
+            .UsePlatfodmDetect()
             .WithInterFont()
             .LogToTrace();
 }
--- D2.UI/ViewLocator.cs
+++ D2.UI/ViewLocator.cs
@@ -17,7 +17,7 @@ public class ViewLocator : IDataTemplate
     {
         if (param is null)
             return null;
-        
+
         var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         var type = Type.GetType(name);
 
@@ -25,7 +25,7 @@ public class ViewLocator : IDataTemplate
         {
             return (Control)Activator.CreateInstance(type)!;
         }
-        
+
         return new TextBlock { Text = "Not Found: " + name };
     }
 
